24b9f52b2d5453a5fdf7b1e4cf8461424d2e4ee1
Keith Roseberry <kroseber@stevens.edu>
2018-03-13 18:07:06 -0400

    Refactored code for finding family of child

--- src/cousins_married.py
+++ src/cousins_married.py
@@ -27,6 +27,22 @@ def log_error(family_id, grand_family_id):
     ErrorLogger.__logError__(ErrorLogger._FAMILY, "US19", family_id, \
         "Marriage is of cousins of family " + grand_family_id)
 
+def find_family_of_child(child_id, families):
+    """
+    ---------------------------------------------------------------------------
+    find_family_of_child
+    --------------------
+    This function finds the family in which the child identifier is in the
+    children array.
+    ---------------------------------------------------------------------------
+    """
+    family_object = None
+    for f_idx in families:
+        for c_id in families[f_idx].children:
+            if child_id == c_id:
+                family_object = families[f_idx]
+    return family_object
+
 def is_marriage_of_cousins(family, families):
     """
     ---------------------------------------------------------------------------
@@ -57,19 +73,11 @@ def is_marriage_of_cousins(family, families):
 
     # Find the previous generation family of the husband (the family that has
     # the husband's parents).
-    gen_1_up_husband = None
-    for f_idx in families:
-        for childId in families[f_idx].children:
-            if family.husbandId == childId:
-                gen_1_up_husband = families[f_idx]
+    gen_1_up_husband = find_family_of_child(family.husbandId, families)
 
     # Find the previous generation family of the wife (the family that has
     # the wife's parents).
-    gen_1_up_wife = None
-    for f_idx in families:
-        for childId in families[f_idx].children:
-            if family.wifeId == childId:
-                gen_1_up_wife = families[f_idx]
+    gen_1_up_wife = find_family_of_child(family.wifeId, families)
 
     # If the husband's parent's family and the wife's parent's family have both
     # been found, then continue.
@@ -78,32 +86,14 @@ def is_marriage_of_cousins(family, families):
         # Find the next generation up families of the father and mother of the
         # husband (the families that have the parents of the husband's father
         # and mother).
-        gen_2_up_husband_father = None
-        for f_idx in families:
-            for childId in families[f_idx].children:
-                if gen_1_up_husband.husbandId == childId:
-                    gen_2_up_husband_father = families[f_idx]
-
-        gen_2_up_husband_mother = None
-        for f_idx in families:
-            for childId in families[f_idx].children:
-                if gen_1_up_husband.wifeId == childId:
-                    gen_2_up_husband_mother = families[f_idx]
+        gen_2_up_husband_father = find_family_of_child(gen_1_up_husband.husbandId, families)
+        gen_2_up_husband_mother = find_family_of_child(gen_1_up_husband.wifeId, families)
 
         # Find the next generation up families of the father and mother of the
         # wife (the families that have the parents of the wife's father and
         # mother).
-        gen_2_up_wife_father = None
-        for f_idx in families:
-            for childId in families[f_idx].children:
-                if gen_1_up_wife.husbandId == childId:
-                    gen_2_up_wife_father = families[f_idx]
-
-        gen_2_up_wife_mother = None
-        for f_idx in families:
-            for childId in families[f_idx].children:
-                if gen_1_up_wife.wifeId == childId:
-                    gen_2_up_wife_mother = families[f_idx]
+        gen_2_up_wife_father = find_family_of_child(gen_1_up_wife.husbandId, families)
+        gen_2_up_wife_mother = find_family_of_child(gen_1_up_wife.wifeId, families)
 
         # Compare if any of the found families in the second generation up are
         # the same. If yes, then the marriage is of cousins.
